6d52332abb0ea53630b21a49670404cabc5dbe6a
Zhuk Stepan <STEP870906@gmail.com>
2022-08-20 00:59:57 +0300

    Update python_test.py

--- tests/python_test.py
+++ tests/python_test.py
@@ -1,7 +1,60 @@
-# testing Fibonacci number function
-def fib(n: int) -> int:
-    return n if n < 2 else fib(n-1)+fib(n-2)
+# pip install selenium
+# pip install webdriver_manager
+# pip install chromedriver-autoinstaller
+from asyncio.windows_events import NULL
+from contextlib import nullcontext
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+import chromedriver_autoinstaller
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+
+chromedriver_autoinstaller.install()  # Check if the current version of chromedriver exists
+                                      # and if it doesn't exist, download it automatically,
+                                      # then add chromedriver to path
+# service = Service(executable_path=ChromeDriverManager().install())
+
+driver = webdriver.Chrome(service=ChromeService(executable_path=ChromeDriverManager().install()))
+
+# driver = webdriver.Chrome(service=service)
+# driver = webdriver.Chrome()
+# driver.get("http://www.python.org")
+# assert "Python" in driver.title
+
+# downloading chromedriver to the test path from https://chromedriver.storage.googleapis.com/index.html?path=104.0.5112.79/
+# driver = webdriver.Chrome("C:/Users/Stepan_Zhuk/Documents/GitHub/reporting/test/chromedriver.exe")
+
+# driver.get("http://selenium.dev")
+# driver.get("https://192.168.59.100:32008")
 
 
-def test_fibonacci():
-    assert fib(10) == 55 
+
+
+driver.get ("https://www.facebook.com")
+driver.find_element(By.ID, 'email').send_keys('fakeemail@crossbrowsertesting.com')
+
+driver.find_element(By.ID, 'pass').send_keys('fakepassword1')
+
+# driver.find_element(By.ID, 'loginbutton').click()
+d = driver.find_element(By.NAME, 'login').click()
+
+# wait the ready state to be complete
+WebDriverWait(driver=driver, timeout=10).until(
+    lambda x: x.execute_script("return document.readyState === 'complete'")
+)
+error_message = "Find your account and log in."
+# get the errors (if there are)
+errors = driver.find_elements(By.LINK_TEXT, "Find your account and log in.")
+
+# print the errors optionally
+# for e in errors:
+#     print(e.text)
+# if we find that error message within errors, then login is failed
+if any(error_message in e.text for e in errors):
+    print("Test PASSED. Login Failed")
+else:
+    print("Test Failed. Login successful")
+
+driver.quit()
