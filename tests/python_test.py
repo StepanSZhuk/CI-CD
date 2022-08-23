b18de3d03841ff6f095457884200ed428655fb7e
Zhuk Stepan <STEP870906@gmail.com>
2022-08-23 16:12:50 +0300

    Update python_test.py

--- tests/python_test.py
+++ tests/python_test.py
@@ -38,9 +38,6 @@ driver.find_element(By.ID, 'email').send_keys('fakeemail@crossbrowsertesting.com
 
 driver.find_element(By.ID, 'pass').send_keys('fakepassword1')
 
-submit_button = driver.find_element(By.NAME, 'login')
-print(submit_button)
-
 # driver.find_element(By.ID, 'loginbutton').click()
 driver.find_element(By.NAME, 'login').click()
 
