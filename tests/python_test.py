2538074c4049fafc258d8f97ef6111950f4557a7
Zhuk Stepan <STEP870906@gmail.com>
2022-08-23 16:59:35 +0300

    Update python_test.py

--- tests/python_test.py
+++ tests/python_test.py
@@ -55,7 +55,7 @@ errors = driver.find_elements(By.LINK_TEXT, "Find your account and log in.")
 # if we find that error message within errors, then login is failed
 
 # assert 'admin' not in driver.page_source
-assert 'Find your account and log in.' not in driver.page_source
+assert 'Find your account and log in.' in driver.page_source
 # if any(error_message in e.text for e in errors):
 #     print("Test PASSED. Login Failed")
 #     print(driver.title)
