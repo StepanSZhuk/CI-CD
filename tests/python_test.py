f9de74684aff38a8ac78efb2747fc7f5d20a59d2
Zhuk Stepan <STEP870906@gmail.com>
2022-08-23 16:39:02 +0300

    Update python_test.py

--- tests/python_test.py
+++ tests/python_test.py
@@ -37,7 +37,9 @@ driver.get ("https://www.facebook.com")
 driver.find_element(By.ID, 'email').send_keys('fakeemail@crossbrowsertesting.com')
 
 driver.find_element(By.ID, 'pass').send_keys('fakepassword1')
-
+####
+submit = driver.find_element(By.NAME, 'login')
+####
 # driver.find_element(By.ID, 'loginbutton').click()
 driver.find_element(By.NAME, 'login').click()
 
