6c43a64cbe33bbc1ed2a6edc4ed242a87ad5868e
Zhuk Stepan <STEP870906@gmail.com>
2022-08-24 17:49:17 +0300

    Update python_test.py

--- tests/python_test.py
+++ tests/python_test.py
@@ -4,10 +4,10 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 import chromedriver_autoinstaller
-from pyvirtualdisplay import Display
-from selenium.webdriver.common.keys import Keys
-display = Display(visible=0, size=(800, 800))  
-display.start()
+# from pyvirtualdisplay import Display
+# from selenium.webdriver.common.keys import Keys
+# display = Display(visible=0, size=(800, 800))  
+# display.start()
 
 
 
