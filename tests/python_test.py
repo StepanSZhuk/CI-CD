f60a82e347ef9cb733434cb7573e10ee2a03e42b
Zhuk Stepan <STEP870906@gmail.com>
2022-08-24 12:27:34 +0300

    Update python_test.py

--- tests/python_test.py
+++ tests/python_test.py
@@ -46,9 +46,8 @@ driver.find_element(By.ID, 'email').send_keys('fakeemail@crossbrowsertesting.com
 driver.find_element(By.ID, 'pass').send_keys('fakepassword1')
 
 # driver.find_element(By.ID, 'loginbutton').click()
-driver.find_element(By.NAME, 'login').click()
+elem = driver.find_element(By.NAME, 'login').click()
 
-elem = driver.find_element(By.NAME, 'login')
 elem.send_keys(Keys.RETURN)
 
 error_message = "Find your account and log in."
