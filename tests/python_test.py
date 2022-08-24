e356c715d144065eab8b483e00d52152e0a8ce32
Zhuk Stepan <STEP870906@gmail.com>
2022-08-24 15:15:06 +0300

    Update python_test.py

--- tests/python_test.py
+++ tests/python_test.py
@@ -22,7 +22,7 @@ options = [
   # Define window size here
    "--window-size=1200,1200",
    "--ignore-certificate-errors",
-   "--headless"
+#    "--headless"
     #"--disable-gpu",
     #"--disable-extensions",
 #    "--no-sandbox",
