f77c7fb88326edcd7d810178908f8c783abf9087
Zhuk Stepan <STEP870906@gmail.com>
2022-08-24 18:20:07 +0300

    Update python_test.py

--- tests/python_test.py
+++ tests/python_test.py
@@ -21,8 +21,8 @@ chrome_options = webdriver.ChromeOptions()
 options = [
   # Define window size here
    "--window-size=1200,1200",
-   "--ignore-certificate-errors",
-   "--headless"
+   "--ignore-certificate-errors"
+#    "--headless"
 #    "--disable-gpu",
 #    "--disable-extensions",
 #    "--no-sandbox"
