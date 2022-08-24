d4931f567a51535d349db851f2336be1d0784af3
Zhuk Stepan <STEP870906@gmail.com>
2022-08-24 23:47:41 +0300

    Update python_test.py

--- tests/python_test.py
+++ tests/python_test.py
@@ -21,12 +21,12 @@ chrome_options = webdriver.ChromeOptions()
 options = [
   # Define window size here
    "--window-size=1200,1200",
-   "--ignore-certificate-errors"
-#    "--headless"
+   "--ignore-certificate-errors",
+   "--headless",
 #    "--disable-gpu",
 #    "--disable-extensions",
 #    "--no-sandbox"
-#    "--disable-dev-shm-usage",
+   "--disable-dev-shm-usage"
 #    "--remote-debugging-port=9222"
 ]
 
