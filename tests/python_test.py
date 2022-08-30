0c4602b3ad4ea93662879cb421d307ce1ab3c253
Zhuk Stepan <STEP870906@gmail.com>
2022-08-30 14:51:56 +0300

    Update python_test.py

--- tests/python_test.py
+++ tests/python_test.py
@@ -99,7 +99,7 @@ driver.find_element(By.NAME, 'login').click()
 # error_message = "Find your account and log in."
 # get the errors (if there are)
 errors = driver.find_elements(By.LINK_TEXT, "Find your account and log in.")
-driver.get_screenshot_as_file("Screen.png")
+driver.get_screenshot_as_file("/tests/Screen.png")
 
 
 # # # print the errors optionally
